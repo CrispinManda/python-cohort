7c23dc4c1e1ce328008f723e4577b978a8a6f173
Crispin Manda <crispinmanda06@gmail.com>
2025-04-03 11:43:39 +0300

    fist commit

---
+++ index.py
@@ -0,0 +1,80 @@
+#variables in python
+#variable is a container for storing data values
+#variables are created when you assign a value to it
+#variables do not need to be declared with any particular type and can even change type after they have been set
+#variables are case-sensitive
+#variable names must start with a letter or underscore character
+#variable names can only contain letters, numbers, or underscores
+#variable names cannot start with a number
+#variable names cannot be a reserved keyword
+#variable names should be descriptive and meaningful
+#variable names should be in lowercase
+#variable names should be separated by underscores
+#variable names should not be too long
+#variable names should not be too short
+
+
+x = 5 #x is a variable that stores the value 5
+y = "Hello, World!" #y is a variable that stores the string "Hello, World!"
+z = 3.14 #z is a variable that stores the float 3.14
+a = True #a is a variable that stores the boolean value True
+b = None #b is a variable that stores the value None
+print(x) #prints the value of x
+print(y) #prints the value of y
+print(z) #prints the value of z
+print(a) #prints the value of a
+print(b) #prints the value of b
+print(type(x)) #prints the type of x
+print(type(y)) #prints the type of y
+print(type(z)) #prints the type of z
+print(type(a)) #prints the type of a
+print(type(b)) #prints the type of b
+
+# lists in python
+#lists are used to store multiple items in a single variable
+#lists are created using square brackets []
+#lists can contain any data type
+#lists can contain duplicate values
+
+fruits = ['apple' , 'orange' , 'mango' , 'orange' , 'pawpaw']
+print(fruits) #prints the list of fruits 
+print(type(fruits)) #prints the type of fruits = list
+print(len(fruits)) #prints the length of the list of fruits
+print(fruits[0]) #prints the first item in the list of fruits
+print(fruits[1]) #prints the second item in the list of fruits
+print(fruits[2]) #prints the third item in the list of fruits
+
+#tuples in python
+#tuples are used to store multiple items in a single variable
+#tuples are created using parentheses ()
+cars= ('Volvo', 'Mercedes' , 'Honda') #tuple
+print(cars) #prints the tuple of cars
+print(type(cars)) #prints the type of cars = tuple
+print(len(cars)) #prints the length of the tuple of cars
+print(cars[0]) #prints the first item in the tuple of cars
+print(cars[1]) #prints the second item in the tuple of cars
+print(cars[0:2]) #prints the first two items in the tuple of cars
+
+students = ('John', 'Mary' , 'Peter') #tuple
+
+# difference between list and tuple
+#lists are mutable (can be changed) while tuples are immutable (cannot be changed) 
+#lists are created using square brackets [] while tuples are created using parentheses ()
+#lists can contain duplicate values while tuples can also contain duplicate values
+#lists can be nested while tuples can also be nested
+#lists can be sliced while tuples can also be sliced
+#lists can be concatenated while tuples can also be concatenated
+#lists can be repeated while tuples can also be repeated
+#lists can be iterated while tuples can also be iterated
+#lists can be unpacked while tuples can also be unpacked
+
+#Dictionaries in python
+#Dictionaries are used to store data values in key:value pairs
+#Dictionaries are created using curly brackets {}
+#Dictionaries can contain any data type
+#Dictionaries can contain duplicate keys
+
+
+
+
+
